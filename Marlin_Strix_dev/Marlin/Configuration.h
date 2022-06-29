6533678da79142219814871780ebb37cd184b280
ng-family <32771826+ng-family@users.noreply.github.com>
2022-06-28 22:10:34 -0600

    Expanded Bed Size to allow overshoot
    Should probably adjust bed offset instead of setting the bed size to the printer's volumetric physical size

--- Marlin_Strix_dev/Marlin/Configuration.h
+++ Marlin_Strix_dev/Marlin/Configuration.h
@@ -1488,8 +1488,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
