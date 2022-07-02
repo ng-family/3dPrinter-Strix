4e5d6879539c0d5e84550fb607c2dc54f7c3b69c
ng-family <32771826+ng-family@users.noreply.github.com>
2022-07-02 13:57:40 -0600

    Added tft firmware update

--- Marlin_Strix_dev/Marlin/Configuration.h
+++ Marlin_Strix_dev/Marlin/Configuration.h
@@ -1011,7 +1011,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 500 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 500 } //Corrected for Anet A8 hardware
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
