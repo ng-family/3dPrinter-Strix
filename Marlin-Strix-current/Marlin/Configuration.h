1a82d5f0a2f0685c45a90a98857f84d1d9aff8fc
ng-family <32771826+ng-family@users.noreply.github.com>
2022-09-11 13:22:30 -0600

    Update Configuration.h
    Added another 5%
    
    Create firmware-v1.2.0.bin

--- Marlin-Strix-current/Marlin/Configuration.h
+++ Marlin-Strix-current/Marlin/Configuration.h
@@ -1165,7 +1165,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 102 } //Corrected for Anet A8 extruder
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 107 } //Corrected for Anet A8 extruder
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
