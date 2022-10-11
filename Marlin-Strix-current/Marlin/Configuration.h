1a214ed2b85d85b82fa009eefdd1f6bcbf290d2d
ng-family <32771826+ng-family@users.noreply.github.com>
2022-10-10 21:17:39 -0600

    Baked print bed origins

--- Marlin-Strix-current/Marlin/Configuration.h
+++ Marlin-Strix-current/Marlin/Configuration.h
@@ -1695,11 +1695,11 @@
 #define Y_BED_SIZE 300
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
-#define Z_MIN_POS 0
+#define X_MIN_POS -30
 #define X_MAX_POS X_BED_SIZE
+#define Y_MIN_POS -9.5
 #define Y_MAX_POS Y_BED_SIZE
+#define Z_MIN_POS 0
 #define Z_MAX_POS 240
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
